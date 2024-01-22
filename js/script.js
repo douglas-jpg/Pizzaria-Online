450a378ab9679771c17be2d4f3a1ad9cd529a689
Douglas Lima <douglaslima13bezerra@gmail.com>
2024-01-22 15:32:38 -0300

    feat: add json-sever e add mecanismo de ordenação e pesquisa das pizzas

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 const cardArea = document.querySelector("#cardArea");
 const loadElement = document.querySelector("#loadElement");
+const select = document.querySelector("#ordemPizzas");
+const formSearch = document.querySelector("#formSearch");
+const inputSearch = document.querySelector("#searchInput");
 
-const URL = "../data/pizzas.json";
+let ordem = "";
+let nome = "";
+
+let URL = `http://localhost:5000/pizza?_sort=${ordem}&nome=${nome}`;
 
 const orderIngredients = function (pizza) {
     let ingredientes = "";
@@ -27,7 +33,7 @@ const createCardPizza = function (pizza) {
                                 <a href="./src/produto.html">
                                     <img src=${pizza.imagem} alt="Pizza de ${pizza.nome}" class="card-img-top pizza mx-auto">
                                 </a>
-                                <div class="card-header">R$ ${pizza.preco}</div>
+                                <div class="card-header">R$ ${pizza.preco},00</div>
                                 <div class="card-body">
                                     <h5 class="card-title">${pizza.nome}</h5>
                                     <p class="card-text truncar-3l">${ingredientes}</p>
@@ -41,17 +47,42 @@ const createCardPizza = function (pizza) {
     cardArea.innerHTML += cardPizza;
 };
 
+
 async function getAllPizzas() {
     const response = await fetch(URL);
     const data = await response.json();
+    console.log(data);
 
     loadElement.classList.add("d-none");
     cardArea.classList.remove("d-none");
 
-    console.log(data);
-    data.map((pizza) => {
-        createCardPizza(pizza);
-    });
+    if (data.length != 0) {
+        data.map((pizza) => {
+            createCardPizza(pizza);
+        });
+    } else {
+        cardArea.innerHTML = `<h3 class="text-danger text-center">Pizza não encontrada</h3>`;
+    }
 }
+
+// melhorar pesquisa por por input
+formSearch.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let searchTerm = inputSearch.value;
+    searchTerm.length == 0
+        ? (nome = "")
+        : (nome = searchTerm[0].toUpperCase() + searchTerm.substring(1));
+
+    URL = `http://localhost:5000/pizza?_sort=${ordem}&nome=${nome}`;
+    cardArea.innerHTML = "";
+    getAllPizzas();
+});
+
+select.addEventListener("change", () => {
+    let ordem = select.value;
+    URL = `http://localhost:5000/pizza?_sort=${ordem}&nome=${nome}`;
+    cardArea.innerHTML = "";
+    getAllPizzas();
+});
 
 getAllPizzas();
