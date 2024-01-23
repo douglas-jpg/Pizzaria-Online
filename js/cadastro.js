657433bdeac25741c9bfb4150a0473685b0e02b9
Douglas Lima <douglaslima13bezerra@gmail.com>
2024-01-23 17:49:59 -0300

    wip: fazendo o script cadastro do cliente

---
+++ js/cadastro.js
@@ -0,0 +1,20 @@
+const formCadastro = document.querySelector("#formCadastrado");
+const firstNameUser = document.querySelector("#nome");
+const lastNameUser = document.querySelector("#sobrenome");
+const phoneUser = document.querySelector("#telefone");
+const cpfUser = document.querySelector("#cpf");
+const dateUser = document.querySelector("#nascimento");
+const emailUser = document.querySelector("#email");
+const passwordUser = document.querySelector("#senha");
+const confirmPassowordUser = document.querySelector("#repetirSenha");
+
+const checkPasswords = function (password1, password2) {
+    if (password1 === password2) {
+        return true;
+    }
+    return false;
+};
+
+formCadastro.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
